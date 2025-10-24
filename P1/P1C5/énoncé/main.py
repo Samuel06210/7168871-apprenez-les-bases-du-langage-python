7885a60c789512abd9161bb525061d09b5080f62
Samuel06210 <qz48ghmbgy@privaterelay.appleid.com>
2025-10-24 15:27:13 +0200

    Update main.py

--- P1/P1C5/énoncé/main.py
+++ P1/P1C5/énoncé/main.py
@@ -1,1 +1,9 @@
-# Écrivez votre code ici !
+nom = "Répinski_samuel"
+age = 43
+taille = 1.90
+est_etudiant = False
+print(f"Je m'apelle {nom}, j'ai {age} ans, je mesure {taille} m et {est_etudiant}
+type(nom)
+type(age)
+type(taille)
+type(est_etudiant)
