51e5f23ea0bcbcfc05348717b9a64b08f06fb74e
Samuel06210 <qz48ghmbgy@privaterelay.appleid.com>
2025-10-30 17:34:14 +0100

    Update main.py

--- P1/P1C7/énoncé/main.py
+++ P1/P1C7/énoncé/main.py
@@ -1,1 +1,7 @@
-# Écrivez votre code ici !
+fruits={}
+fruits={"pomme:rouge", "banane:jaune", "orange:orange"}
+fruits["kiwi"]="vert"
+fruits[banane]
+couleur_banane=fruits["banane"]
+fruits["pomme"]="vert"
+del fruits["banane"]
